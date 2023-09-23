e6700f0a6641e08acc48f710c2c083c901b03322
DESKTOP-VCJF8F8\vinhn <vinhtrungngo1907@gmail.com>
2023-09-23 09:29:34 +0700

    utils

--- model/vehicle.py
+++ model/vehicle.py
@@ -2,13 +2,17 @@ from email.policy import default
 from mongoengine.document import Document
 from mongoengine.fields import EnumField, LongField, ListField, StringField, DateTimeField, DictField
 from mongoengine.errors import ValidationError
-from db_uploader.utils import convertLocalTimeToUTCTime
+from utils import convertLocalTimeToUTCTime
 from bson.objectid import ObjectId
 from datetime import datetime
 from enum import Enum
 import time
 
 _MAX_SIZE_IMAGES = 2
+
+def RetricLengthMaxImages(list_image):
+    if(len(list_image) > _MAX_SIZE_IMAGES):
+        raise ValidationError("List images exceed max size: ", _MAX_SIZE_IMAGES)
 
 class VehicleTypes(Enum):
     CAR = 'Car'
@@ -21,4 +25,6 @@ class VehicleTypes(Enum):
         if(len(list_image) > _MAX_SIZE_IMAGES):
             raise ValidationError("List images exceed max size: ", _MAX_SIZE_IMAGES)
         
-# class Vehicles(Document):
+class Vehicles(Document):
+    camera_id = StringField(required=True)
+    video_id = StringField(required=True)
