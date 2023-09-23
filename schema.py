9d8453a82407ce5c8891a041c56c9cbbb0dc7e16
DESKTOP-VCJF8F8\vinhn <vinhtrungngo1907@gmail.com>
2023-09-23 11:33:47 +0700

    import some modules in schema

--- schema.py
+++ schema.py
@@ -1,4 +1,15 @@
-from dataclasses import dataclass
+from cProfile import label
+from dataclasses import dataclass, field
+from os import PathLike
+from datetime import datetime
+import string
+from typing import List, Dict
+from venv import create
+# from numpy import integer, ndarray
+from base64 import b64encode
+# from cv2 import imencode
+from enum import Enum
+
 @dataclass
 class PlateInfo:
     label: str
