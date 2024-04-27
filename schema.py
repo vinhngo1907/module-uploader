0a416fb74919ceafeb0e24230c3bb5d3c1bed21e
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-04-27 12:58:24 +0700

    new codes

--- schema.py
+++ schema.py
@@ -5,10 +5,11 @@ from datetime import datetime
 import string
 from typing import List, Dict
 from venv import create
-# from numpy import integer, ndarray
+from numpy import integer, ndarray
 from base64 import b64encode
-# from cv2 import imencode
+from cv2 import imencode
 from enum import Enum
+
 
 class VehicleTypes(Enum):
     CAR = "Car"
@@ -16,8 +17,10 @@ class VehicleTypes(Enum):
     BUS = "Bus"
     BICYCLE = "Bicycle"
 
+
 _JPB_BASE64_HEADER = "data:image/jpeg;base64,"
 _JPG_EXT = ".jpg"
+
 
 @dataclass
 class PlateInfo:
@@ -26,3 +29,27 @@ class PlateInfo:
 
     def asDict(self):
         return vars(self)
+
+
+@dataclass
+class ImageInfo:
+    asset_id: str
+    public_id: str
+    url: str
+    secure_url: str
+    format: str
+    created_at: datetime
+
+    def asDict(self):
+        return vars(self)
+
+
+@dataclass
+class UploadImage:
+    jpeg_base64: str
+    folder: PathLike
+
+    @staticmethod
+    def image_base64_encode(bgr_img: ndarray):
+        print(">>>>", _JPB_BASE64_HEADER + b64encode(imencode(_JPG_EXT, bgr_img)))
+        return _JPB_BASE64_HEADER + b64encode(imencode(_JPG_EXT, bgr_img))[1]
--- uploader.py
+++ uploader.py
@@ -7,6 +7,9 @@ from mongoengine.errors import ValidationError
 
 import cloudinary_upload
 from config import DB_NAME, MONGODB_TEST_DB_URI, MONGODB_URI
+from model.images import Images
+from model.record_videos import RecordVideos
+from schema import ImageInfo
 
 # conn
 conn = pymongo.MongoClient(MONGODB_URI)
@@ -15,6 +18,7 @@ database = conn[DB_NAME]
 # conn for mongo engine
 connect(host=MONGODB_TEST_DB_URI)
 
+
 def save_image(image: 64, str, link_folder: str):
     """Upload image in JPEG base 64 format to cloudinary, then upload Image data to Mongo DB
     Return:
@@ -22,7 +26,25 @@ def save_image(image: 64, str, link_folder: str):
     """
 
     try:
-        # response_image = cloudinary
-        print("Hello word")
+        response_image = cloudinary_upload.upload_image(image, link_folder)
+        image_data = ImageInfo(
+            asset_id=response_image["asset_id"],
+            public_id=response_image["public_id"],
+            url=response_image["url"],
+            secure_url=response_image["secure_url"],
+            format=response_image["format"],
+            created_at=response_image["created_at"],
+        )
+
+        return ImageInfo.asDict()
     except Exception as exp:
         return None
+
+
+def set_video_get_id(video_record: RecordVideos):
+    try:
+        video_data: RecordVideos = video_record.save()
+        print({video_record})
+        return video_data.id
+    except ValidationError:
+        return Exception("Record video data format is incorrect")
